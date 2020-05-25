a89685f70cbc9792e7ac4d83127d7a41f5c80d48
Victor Cuspinera <vcuspinera@gmail.com>
2020-05-25 14:16:51 -0700

    Add twitter-search file

--- src/twitter-search.py
+++ src/twitter-search.py
@@ -5,17 +5,20 @@ import sys
 from twitter import *
 
 config = {}
-execfile("../keys/twitter_config.py")
+# execfile("config.py", config)
+# execfile("twitter_config.py", config)
+execfile("../keys/twitter_config.py", config)
 
-twitter = Twitter(
-    auth = OAuth(config["access_key"], config["access_secret"], 
-    config["consumer_key"], config["consumer_secret"])
-)
+twitter = twitter(auth = OAuth(config["access_token_key"], config["access_token_secret"], config["consumer_key"], config["consumer_secret"]))
+
+# twitter = Twitter(
+#     auth = OAuth(config["access_key"], config["access_secret"],
+#     config["consumer_key"], config["consumer_secret"]))
 
 query = twitter.search.tweets(q = sys.argv[1])
 
-#print "Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"])
+print ("Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"]))
 
 for result in query["statuses"]:
-    #print "(%s) @%s %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
-    print (result["text"])
+    print ("(%s) @%s %s" % (result["created_at"], result["user"]["screen_name"], result["text"]))
+    # print (result["text"])
